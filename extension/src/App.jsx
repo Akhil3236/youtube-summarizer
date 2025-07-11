9128f183d14da916c4825b9174efbf56bbf57e13
Akhil <akhiltuluri123@gmail.com>
2025-07-11 13:27:06 +0530

    setup for extension

---
+++ extension/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+
+   
+     <div className='new_word'>
+      <h2>YouTube Enhancer Pro</h2>
+      <p>Extension is ready</p>
+    </div>
+
+
+    </>
+  )
+}
+
+export default App
